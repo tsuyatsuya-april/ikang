af8e942ab2c1764f67ed7b5de52022bec5c44256
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-10-28 17:11:07 +0900

    urlバリデーション実装

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,6 +5,7 @@ class Shop < ApplicationRecord
   has_one :shop_decision, dependent: :destroy
 
   validates :shop_name, presence: true
-
+  url_reg = /\Ahttp(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?/
+  validates :shop_url, format: { with: url_reg, message: '正式なurlの添付をお願いします' }
   
 end
