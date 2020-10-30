4f048a06c2877427acc38cb1a4eef82fcab373e6
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-10-30 13:56:25 +0900

    ゲストユーザー変更制限修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
 
   def check_guest
-    if resource.email == 'guest@example.com'
+    if resource.email == 'test@mail.com'
       redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
     end
   end
